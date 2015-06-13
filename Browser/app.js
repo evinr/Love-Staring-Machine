842f0b02c6a2b58ee238f41c4ba27eff24341b30
Evin Revello <Evin@EvinRevello.com>
2015-06-13 11:34:17 -0700

    Bunch of progress made

--- Browser/app.js
+++ Browser/app.js
@@ -1,38 +1,71 @@
- var seconds = 59;
- var minutes = 3; 
- var score = []; 
+var seconds = 59;
+var minutes = 3; 
+var score = []; 
+var value1;
+var value2;
+var sliceCount = 0;
+var p1data = [];
+var p2data = [];
+
 
  function aggregateAverage () {
-    var average;
-    ///////Blocked by lack of python gerated timeseries data
-    //creates the average
+    var average = []; //holds a list of all of the player scores
+    //calculates the average score of all players
     //writes the average to localstorage
-    localStorage.setItem("totalparticipants", average); 
+    var allScores = localStorage.getItem("cumulativescores"); 
+    average =   allScores.reduce(function(previousValue, currentValue, index, array) {
+                    return previousValue + currentValue;
+                })/allScores.length;
+    document.getElementById("average").innerHTML = ("The Average Score is: " + average);
+ }
+
+function timeSlice () {
+    p1data[sliceCount] = value1;
+    p2data[sliceCount] = value2;
+    sliceCount ++;
+    //Store 240 time slices (one per second) for both players
+    //this data is used for modeling the results
+    //creates a baseline to determine if disconnet has occured
+    //writes the list of time slices into localstorage
+    localStorage.setItem("player1slices", p1data); 
+    localStorage.setItem("player2slices", p2data); 
  }
 
  function totalPoints () {
     var newTotal;
-    ///////Blocked by lack of python gerated timeseries data
     //create a rolling total
     //writes the average to localstorage
-    localStorage.setItem("totalparticipants", newTotal); 
+    localStorage.setItem("totalparticipants", newTotal);
+    localStorage.setItem("totalparticipants", newTotal);
  }
 
  function contactInfo () { //grabs the contact details and transforms them into a combination name and stores everything to local storage
     var coupleName, name, email;
     nameTemp = $('#form').serialize();
     chopped = nameTemp.split("&");
-    nameData = chopped[0].split("=")[1];
-    otherName = chopped[1].split("=")[1];
-    coupleName = nameData + otherName;
-    ///////for testing purposes we are going to use both fiedls for two names to make the mashup logic work solid REMOVE ME
+    nameOne = chopped[0].split("=")[1];
+    nameTwo = chopped[1].split("=")[1];
+    //coupleName = nameOne + nameTwo;
     //does a mashup of the names, giving a few options
+
+    coupleName = this.nameMash (nameOne, nameTwo);
     /////split before and after the inner vowels
  
     document.getElementById("couple-name").innerHTML = ("Couple Name: " + coupleName);
     //writes to local storage
+    localStorage.setItem("player1", nameOne); 
+    localStorage.setItem("player2", nameTwo); 
     localStorage.setItem("couplename", coupleName); 
     //
+ }
+
+ function nameMash (p1,p2) {
+    if (p1.length > 2 && p2.length > 2){
+        return p1.substring(0,1).toUpperCase() + p1.substring(1,Math.floor(p1.length/2)).toLowerCase() + p2.substring(Math.floor(p2.length/2),p2.length).toLowerCase();
+    }
+    else {
+        return p1.substring(0,1).toUpperCase() + p1.substring(1, p1.length).toLowerCase() + p2.toLowerCase();
+    }
  }
 
  function totalParticipants () {
@@ -47,20 +80,20 @@
     var cleanDate;
     cleanDate = new Date().toLocaleString()
     document.getElementById("date-time").innerHTML = ("Date and Time: " + cleanDate); 
-
     localStorage.setItem("timestamp", cleanDate); 
  }
 
  function dataLoader(){
-    $.getJSON("./result.json", function(data) {
-    	score[0] = data["player1"];
-        score[1] = data["player2"];
+    $.getJSON("../result.json", function(data) {
+    	value1 = data["player1"];
+        value2 = data["player2"];
     });
  }
 
  function scoreRender () {
-    document.getElementById("player-1-score").innerHTML = (score[0]);
-    document.getElementById("player-2-score").innerHTML = (score[1]);
+
+    document.getElementById("player-1-score").innerHTML = value1 ? value1 : "Loading...";
+    document.getElementById("player-2-score").innerHTML = value2 ? value2 : "Loading...";
 
     //sets the high scoreRender
     document.getElementById("high-score").innerHTML = ("The High Score is: " + localStorage.getItem("highscore"));
@@ -89,30 +122,40 @@ function highScore () {
     if (score > tempScore) {
     localStorage.setItem("highscore", score); 
     }
-
 }
 
 function initialization () {
     //initializes the persistant dashboard metrics
     localStorage.setItem("highscore", 0);
     localStorage.setItem("totalparticipants", 0);
+    localStorage.setItem("average", 0);
+
+}
+
+function onRefresh () {
+    //initializes the persistant dashboard metrics
+    document.forms[0].elements[0].focus();
+    $('#form')[0].reset();
 }
 
 function dataRender() {//keep bulk of execution of the app within this function
     if (localStorage.getItem("highscore") == null) {
         init = initialization();
     }
+    //display the aggregated average
+    //
+    refresh = onRefresh();
     people = totalParticipants();
     stamp = datesTimes();
+    scoreRender();
+    slice = setInterval(timeSlice, 1000);
     loop = setInterval(dataLoader, 1000);
     countdown = setInterval(myTimer,1000);
-    points = setInterval(scoreRender,633);
+    points = setInterval(scoreRender, 633);
     leader = setInterval(highScore,1000);
 }
 
 $(document).ready(function() {
-    document.forms[0].elements[0].focus();
-    $('#form')[0].reset();
 	dataRender();
 });
 
